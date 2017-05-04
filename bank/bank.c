123f172fb424da6262af6d5595b87cc332398717
Aditya Ayyakad <njadtiya@gmail.com>
2017-05-04 13:45:30 -0400

    Added card write

--- bank/bank.c
+++ bank/bank.c
@@ -61,7 +61,7 @@ void bank_process_local_command(Bank *bank, char *command, size_t len)
     char * p = strtok(command, " \n");
 
     if (strcmp(p, "create-user") == 0) {
-      char *username, *pin_st, *balance_str;
+      char *username, *pin_str, *balance_str;
       int pin, balance;
 
       p = strtok(NULL, " \n");
@@ -74,11 +74,57 @@ void bank_process_local_command(Bank *bank, char *command, size_t len)
       pin = atoi(pin_str);
       balance = atoi(balance_str);
 
-      if (username == NULL || pin_str == NULL || balance_str == NULL || pin < 0 || (pin - 9999) > 0 || balance < 0) {
+      if (username == NULL || strlen(username) > 250 || pin_str == NULL || balance_str == NULL || pin < 0 || (pin - 9999) > 0 || balance < 0) {
         printf("Usage: create-user <user-name> <pin> <balance>\n");
       } else {
-        printf("Success: create-user user-name: %s, pin: %s, balance: %s\n", username, pin, balance);
+        printf("Success: create-user user-name: %s, pin: %d, balance: %d\n", username, pin, balance);
+        char card_filename[255];
+        memset(card_filename, '\0', 255);
+        strcat(card_filename, username);
+        strcat(card_filename, ".card");
+
+        FILE *cardfp = fopen(card_filename, "w+");
+        if (cardfp == NULL) {
+          printf("Error creating card file for user %s\n", username);
+        }
+
+        char *content = username;
+        fprintf(cardfp, "%s\n", content);
+
+        fclose(cardfp);
+
+        printf("Created user %s\n", username);
       }
+    } else if (strcmp(p, "deposit") == 0) {
+      char *username, *amt_str;
+      int amt;
+
+      p = strtok(NULL, " \n");
+      username = p;
+      p = strtok(NULL, " \n");
+      amt_str = p;
+
+      amt = atoi(amt_str);
+
+      if (username == NULL || strlen(username) > 250 || amt_str == NULL || amt < 0) {
+        printf("Usage: deposit <user-name> <amt>\n");
+      } else {
+        printf("Success: deposit\n");
+      }
+
+    } else if (strcmp(p, "balance") == 0) {
+      char *username;
+
+      p = strtok(NULL, " \n");
+      username = p;
+
+      if (username == NULL || strlen(username) > 250) {
+        printf("Usage: balance <user-name>\n");
+      } else {
+        printf("Success: balance\n");
+      }
+    } else {
+      printf("Invalid command\n");
     }
 }
 
